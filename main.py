ceeff704dd9f6f94b8d2aa4862097f4fb5193fd0
avionot244 <aymericvinot38@gmail.com>
2024-02-15 13:25:46 +0100

    new way to fin e tune Bert

--- main.py
+++ main.py
@@ -1,74 +1,70 @@
-from transformers import AutoTokenizer
-from datasets import load_dataset
-from transformers import AutoModelForSequenceClassification
-from transformers import TrainingArguments
-import numpy as np
-import evaluate
-from transformers import Trainer
+import torch
+from transformers import BertTokenizer, BertModel
 
-def tokenize_function(examples):
-    """
-    This function takes a set of sentances as a parameter and returns the tokenized sentances
+import logging
+import matplotlib.pyplot as plt
 
-    Param
-    -------
-        examples : list of string
-    """
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+tokenizer : BertTokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+text = "Aatrox is a good champion for the toplane role and is also one of the top champions in the tierlists"
 
-def compute_metrics(eval_pred, metric):
-    """
-    Compute the accuracy of a given prediction with a metrics
+# Add the special markers
+marked_text = "[CLS]" + text + " [SEP]"
 
-    Param
-    --------
-        eval_pred : tuple that gives the predictions and the labels of the the prediction
-        metric : metric used to evaluate the accurary of our prediction
-    """
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+# Split the sentence into tokens.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Map the token strings to their vocabulary indeces
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+# Mark each of the 22 tokens as belonging to sentence "1".
+segments_ids = [1] * len(tokenized_text)
+
+tokens_tensor : torch.Tensor = torch.tensor([indexed_tokens])
+segments_tensors = torch.tensor([segments_ids])
 
 
-if __name__ == "__main__":
-    # Getting our tokenizer
-    tokenizer : AutoTokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+# Loading our bert model
+model : BertModel = BertModel.from_pretrained("bert-base-uncased",
+                                  output_hidden_states = True)
 
-    # Loading our dataset from hugging face
-    print("Getting dataset")
-    dataset = load_dataset("yelp_review_full")
-    print("Tokenizing dataset")
-    tokenized_datasets = dataset.map(tokenize_function, batched=True)
-    small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
-    small_eval_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+model.eval()
 
-    print(small_train_dataset)
-    print(small_eval_dataset)
+# Run the text through BERT, and collect all of the hidden states produced
+# from all 12 layers.
+with torch.no_grad():
+    
+    outputs = model(tokens_tensor, segments_tensors)
 
-    # Getting our pretrained model
-    print("Getting pretrained BERT model")
-    model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=5)
+    # Evaluating the model will return a different number of objects based on 
+    # how it's  configured in the `from_pretrained` call earlier. In this case, 
+    # becase we set `output_hidden_states = True`, the third item will be the 
+    # hidden states from all layers. See the documentation for more details:
+    # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
 
-    # Getting our training hyperparameters
-    print("Getting training hyperparameters from the model")
-    training_args = TrainingArguments(output_dir="./test_trainer")
+    hidden_states = outputs[2]
 
-    # Getting our evaluate class to evaluate our model during training
-    print("Getting our evaluate class for training")
-    metric = evaluate.load("accuracy")
+# hidden_states is a 4 dimensional array that stores
+# The Layer Number
+# The batch number (1 in our case because we have one sentance)
+# The word/token number
+# The hidden unit/feature number
 
-    # Creating our trainger 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=small_train_dataset,
-        eval_dataset=small_eval_dataset,
-        compute_metrics=compute_metrics
-    )
+print ("Number of layers:", len(hidden_states), "  (initial embeddings + 12 BERT layers)")
+layer_i = 0
 
-    print("Training our model")
-    trainer.train()
+print ("Number of batches:", len(hidden_states[layer_i]))
+batch_i = 0
 
+print ("Number of tokens:", len(hidden_states[layer_i][batch_i]))
+token_i = 0
 
+print ("Number of hidden units:", len(hidden_states[layer_i][batch_i][token_i]))
+
+toekn_i = 5
+layer_i = 5
+vec = hidden_states[layer_i][batch_i][token_i]
+
+plt.figure(figsize=(10, 10))
+plt.hist(vec, bins=200)
+plt.savefig("Range_of_values_of_the_hidden_unit.png")
